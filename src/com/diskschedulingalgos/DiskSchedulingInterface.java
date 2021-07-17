8bca7d9bce18a9ead604fc51be7b0a28a33f07c1
Gaiban_khan <gaiban.khan.144.gk@gmail.com>
2021-07-17 10:20:58 +0530

    Removed an unused import statement

--- src/com/diskschedulingalgos/DiskSchedulingInterface.java
+++ src/com/diskschedulingalgos/DiskSchedulingInterface.java
@@ -2,7 +2,6 @@ package com.diskschedulingalgos;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Objects;
 
 public interface DiskSchedulingInterface {
